49f65252a2009a0f83b89dd33aa889924f505325
Evan Simons <esimons@ebsco.com>
2016-01-21 11:08:44 -0500

    Adding redux devtools panel.

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -5,18 +5,39 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './components/App';
 
-import { createStore } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
 import reducer from './reducers/reducer';
 
+// Redux Devtools
+import {createDevTools} from 'redux-devtools';
+import LogMonitor from 'redux-devtools-log-monitor';
+import DockMonitor from 'redux-devtools-dock-monitor';
 
-var store = createStore(reducer);
+const DevTools = createDevTools(
+	<DockMonitor toggleVisibilityKey="ctrl-h" changePositionKey="ctrl-q">
+		<LogMonitor theme="tomorrow" />
+	</DockMonitor>
+);
+
+const finalCreateStore = compose(
+	applyMiddleware(
+		// MIDDLEWARES GO 'ERE
+	),
+	DevTools.instrument()
+)(createStore);
+
+const store = finalCreateStore(reducer);
 
 // import { updateText } from './actions/actions.js';
 // store.dispatch(updateText('ahoy!'));
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App className='foo' />
-  </Provider>,
-  document.getElementById('app')
+const Main = (
+	<Provider store={store}>
+		<div>
+			<App className='foo' />
+			<DevTools />
+		</div>
+	</Provider>
 );
+
+ReactDOM.render(Main, document.getElementById('app'));
