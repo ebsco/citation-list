673a3cdf48f18d1641ea5111f53d19f390f2278b
gjjones <gjjones@ebsco.com>
2016-01-22 10:47:30 -0500

    allows edits of temporary result list for hiding in citation mode.

--- public/scripts/containers/ResultListToolbar.js
+++ public/scripts/containers/ResultListToolbar.js
@@ -3,8 +3,11 @@ import React, { Component } from 'react';
 
 import { Link } from 'react-router';
 
-const ResultListToolbar = (props) => (
+const ResultListToolbar = ({rangeStart, rangeEnd, totalCount}) => (
   <div>
+    <h2>
+      Search results: {rangeStart} - {rangeEnd} of {totalCount}
+    </h2>
     <Link to="/citation-list">Get Citations</Link>
   </div>
 )
